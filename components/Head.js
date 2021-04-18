dce2f2de64260c7ad4075474c0b758721b3a5d65
geenva <geenva@users.noreply.github.com>
2021-04-18 17:02:10 +0800

    refactor(Head)

---
+++ components/Head.js
@@ -0,0 +1,32 @@
+import Head from "next/head";
+
+export default function Headd() {
+  return (
+    <Head>
+      <title>Matt_Lawz</title>
+      <link rel="icon" href="/big floppa.gif" />
+      <meta
+        name="dmca-site-verification"
+        content="VDZYNnVkTjB6Y2daTDg2NWw1Vmk1Zz090"
+      />
+      <meta name="og:type" content="website" />
+      <meta
+        name="description"
+        content="Matt's portfolio that is built with NextJs"
+      />
+      <meta name="og:title" content="MattLawz" />
+      <meta
+        name="description"
+        content="Matt's portfolio that is built with NextJs"
+      />
+      <meta name="keywords" content="fullstack developer, mattlawz, nextjs" />
+      <meta name="og:url" content="https://mattlawz.dev/" />
+      <meta
+        name="og:description"
+        content="Matt's portfolio that is built with NextJs"
+      />
+      <meta name="og:image" content="/big floppa.gif" />
+      <script async src="https://arc.io/widget.min.js#Zn9FDne2"></script>
+    </Head>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import Head from "next/head";
+import Headd from "../components/Head";
 import styles from "../styles/Home.module.css";
 import Image from "next/image";
 import Link from "next/link";
@@ -6,33 +6,7 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div className={styles.container}>
-      <Head>
-        <title>Matt_Lawz</title>
-        <link rel="icon" href="/big floppa.gif" />
-        <meta
-          name="dmca-site-verification"
-          content="VDZYNnVkTjB6Y2daTDg2NWw1Vmk1Zz090"
-        />
-        <meta name="og:type" content="website" />
-        <meta
-          name="description"
-          content="Matt's portfolio that is built with NextJs"
-        />
-        <meta name="og:title" content="MattLawz" />
-        <meta
-          name="description"
-          content="Matt's portfolio that is built with NextJs"
-        />
-        <meta name="keywords" content="fullstack developer, mattlawz, nextjs" />
-        <meta name="og:url" content="https://mattlawz.dev/" />
-        <meta
-          name="og:description"
-          content="Matt's portfolio that is built with NextJs"
-        />
-        <meta name="og:image" content="/big floppa.gif" />
-        <script async src="https://arc.io/widget.min.js#Zn9FDne2"></script>
-      </Head>
-
+      <Headd />
       <main className={styles.title}>
         <h1 className={styles.title}>MattLawz</h1>
         <p className={styles.description}>Fullstack Developer</p>
